186e8162305ebc50aad8c62b9bed9b3694637579
matthias245 <matthias.scheungraber@gmx.de>
2022-04-01 12:14:41 +0200

    restcalls v2

--- 1.Vorlesung/restcall.js
+++ 1.Vorlesung/restcall.js
@@ -1,14 +1,22 @@
 var https = require('https');
+const { getSystemErrorMap } = require('util');
 
 const options = {
-    path: '/82.165.229.152/json/',
+    path: '/195.37.242.201/json/',
     host: 'ipapi.co',
     port: 443,
-    headers: {'User-Agent':'nodejs-ipapi-v1.02'}
+    headers: { 'User-Agent': 'nodejs-ipapi-v1.02' }
 };
 
 https.get(options, function(resp){
     var body = '';
-
-
+    resp.on('data', function(data){
+        body += data;
+    }
+    );
+resp.on('end', function(){
+    var loc = JSON.parse(body);
+    console.log(loc);
+    console.log("Die IP-Adresse ist in " + loc.city + " beheimatet.")
+    });
 });
