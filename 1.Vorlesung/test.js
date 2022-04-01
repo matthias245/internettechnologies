4064a3b3c33b449466d6a9dbf8dac30509984a26
matthias245 <matthias.scheungraber@gmx.de>
2022-04-01 09:22:08 +0200

    1.Vorlesung: test.js fertig

--- 1.Vorlesung/test.js
+++ 1.Vorlesung/test.js
@@ -1,8 +1,35 @@
+/**
+ * Einführung Java Script.
+ * @author Hias
+ */
+
+/**
+ * Variable a used for addition.
+ */
 var a = 3;
+/**
+ * Variable b used for addition.
+ */
 var b = 30;
 
 /**
- * Print the result of a+b to console.
+ * Array including all students.
  */
+var students = ["Benedikt", "Matthias", "Mattis", "Stefan", "Marcel", "Bohdan"]
+
+/**
+ * New Map.
+ */
+ let fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+  ]);
+
+
 let c = a+b;
-console.log(c);
+console.log("\n--------------------------------");
+console.log(a + " + " + b + " = " + c);
+console.log("Ein normales Array: " + students[0] + " " + students[1]);
+console.log("Das Gewicht eines Apfels: " + fruits.get("apples"));
+console.log("--------------------------------\n");
